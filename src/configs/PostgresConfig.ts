6c63828c10bf2f7ea09ebb020553a0a34e6c23c0
t.gusev <gusev.t.s1@gmail.com>
2026-03-13 20:12:18 +0300

    c

--- src/configs/PostgresConfig.ts
+++ src/configs/PostgresConfig.ts
@@ -13,7 +13,7 @@ export function loadPostgresConfig(): PostgresConfig {
   const password = process.env.POSTGRES_PASSWORD ?? "GdftXsks8eN6L";
   const host = process.env.POSTGRES_HOST ?? "localhost";
   const port = process.env.POSTGRES_PORT ?? "5432";
-  const database = process.env.POSTGRES_DATABASE ?? "nuts";
+  const database = process.env.POSTGRES_DATABASE ?? "nuts-poker";
   const url =
     process.env.POSTGRES_URL ??
     `postgres://${encodeURIComponent(user)}:${encodeURIComponent(password)}@${host}:${port}/${database}`;
